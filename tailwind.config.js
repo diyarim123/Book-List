c3908237055860ee87a542a8dce422e7996d930e
Diyari Oct <diyarimohammed33@gmail.com>
2023-07-02 22:17:42 +0300

    Changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./dist/**.{html,js,css}"],
+  content: ["./public/**.{html,js,css}"],
   theme: {
     extend: {},
   },
